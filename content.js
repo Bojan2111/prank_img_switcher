f9a13cfe12fcabba8f36be61db9e630f8992687c
Bojan Adzic <39236370+Bojan2111@users.noreply.github.com>
2024-12-09 09:18:01 +0100

    Update content.js

--- content.js
+++ content.js
@@ -1,9 +1,12 @@
-// Wait for the DOM to load
-document.addEventListener("DOMContentLoaded", () => {
+const observer = new MutationObserver(() => {
   // Replace this selector with the one for the target image
-  const targetImage = document.querySelector("img");
+  const targetImage = document.querySelector("img#logo");
 
   if (targetImage) {
-    targetImage.src = chrome.runtime.getURL("prank-image.png");
+    targetImage.src = "https://www.openelearning.org/free-openclipart-elearning/clipart-database/www_Sticker_Tk/www_Sticker_Tk_kiss_my_ass.svg";
+    targetImage.style = "background-color: white;";
+    observer.disconnect();
   }
 });
+
+observer.observe(document.body, { childList: true, subtree: true });
